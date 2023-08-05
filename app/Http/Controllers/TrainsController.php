d2e470b530dff4f3b85f8a66fb6b52f1766604b6
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-08-05 23:22:22 +0200

    dd trains aggiunto

--- app/Http/Controllers/TrainsController.php
+++ app/Http/Controllers/TrainsController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Train;
+
 class TrainsController extends Controller
 {
-    //
+    public function index (){
+
+        $trains = Train::where('data_partenza', '>=', now ())->get();
+        dd($trains);
+         return view('welcome');
+    }
 }
